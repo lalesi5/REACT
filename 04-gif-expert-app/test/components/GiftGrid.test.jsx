4b0502c4a10d81ce04cdf1a239bdb4d7bc1e0b83
Alex Carrera <a_fcalvarez@hotmail.com>
2023-04-20 15:38:01 -0500

    fin testing GiftGrid

--- 04-gif-expert-app/test/components/GiftGrid.test.jsx
+++ 04-gif-expert-app/test/components/GiftGrid.test.jsx
@@ -1,5 +1,9 @@
 const { render, screen } = require("@testing-library/react");
 const { GifGrid } = require("../../src/components/GifGrid");
+const { useFetchGifts } = require('../../src/hooks/useFetchGifts')
+
+
+jest.mock('../../src/hooks/useFetchGifts');
 
 describe('Pruebas en GiftGrid', () => {
 
@@ -7,10 +11,44 @@ describe('Pruebas en GiftGrid', () => {
 
     test('debe mostrar el loading inicialmente ', () => {
 
+        useFetchGifts.mockReturnValue({
+            images: [],
+            isLoading: true
+        });
+
         render(<GifGrid category={category} />);
         expect(screen.getByText('Cargando....'));
         expect(screen.getByText(category));
         //screen.debug();
 
-    })
+    });
+
+
+    test('debe de mostrar items cuando se cargan las imagenes mediante useFetchGifts ', () => {
+
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'Saitma',
+                url: 'https://localhost/saitama.jpg'
+            },
+            {
+                id: '123',
+                title: 'Goku',
+                url: 'https://localhost/goku.jpg'
+            },
+        ]
+
+        useFetchGifts.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+
+        render(<GifGrid category={category} />);
+        screen.debug();
+
+        expect(screen.getAllByRole('img').length).toBe(2);
+
+
+    });
 })
---
+++ 04-gif-expert-app/test/hooks/useFetchGifts.test.js
@@ -0,0 +1,35 @@
+import { renderHook, waitFor } from "@testing-library/react"
+import { useFetchGifts } from "../../src/hooks/useFetchGifts"
+
+describe('Pruebas sobre useFetchGifts', () => {
+
+    test('debe de regresar el estado inicial', () => {
+
+        const { result } = renderHook(() => useFetchGifts('One Punch'));
+        // console.log(result);
+        const { images, isLoading } = result.current;
+
+        expect(images.length).toBe(0);
+        expect(isLoading).toBeTruthy();
+
+    });
+
+    test('debe de retornar un arreglo de imagenes y isLoading en false', async () => {
+
+        const { result } = renderHook(() => useFetchGifts('One Punch'));
+
+        await waitFor(
+            () => expect(result.current.images.length).toBeGreaterThan(0)
+        );
+
+        const { images, isLoading } = result.current;
+
+
+        expect(images.length).toBeGreaterThan(0);
+        expect(isLoading).toBeFalsy();
+
+    });
+
+
+
+})
